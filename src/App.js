60571ccda0b4d9ed675863f91a843b78e62d0205
“Duy <pdk202016@gmail.com>
2024-09-17 21:35:47 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword/ResetPassword";
+import InputCode from "./pages/InputCode/InputCode";
+import UploadNFTPage from "./pages/UploadNFT/UploadNFT.jsx";
+import SubscriptionPage from "./pages/SubscriptionPage/SubscriptionPage.jsx";
+import Account from "./pages/account/account.jsx";
+import CreatePage from "./pages/CreatePage/CreatePage.jsx";
+import SmartContractForm from "./pages/SmartContractForm/SmartContractForm.jsx";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/resetPassword" element={<ResetPassword/>} />
+        <Route path="/inputCode" element={<InputCode/>} />
+        <Route path="/UploadNFTPage" element={<UploadNFTPage/>} />
+        <Route path="/Subscription" element={<SubscriptionPage/>} />
+        <Route path="/Account" element={<Account/>} />
+        <Route path="/CreatePage" element={<CreatePage/>} />
+        <Route path="/SmartContractForm" element={<SmartContractForm/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
