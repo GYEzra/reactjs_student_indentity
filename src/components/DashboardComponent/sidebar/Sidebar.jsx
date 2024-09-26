632f50b4e3d1c06b0af5cf76c83d471792ddc425
thu <165041915+ThuPham60232004@users.noreply.github.com>
2024-09-26 18:33:39 +0700

    THU

--- src/components/DashboardComponent/sidebar/Sidebar.jsx
+++ src/components/DashboardComponent/sidebar/Sidebar.jsx
@@ -1,23 +1,24 @@
 import "./sidebar.scss";
 import HomeIcon from "@mui/icons-material/Home";
 import ShowChartIcon from "@mui/icons-material/ShowChart";
-import SmartToyIcon from "@mui/icons-material/SmartToy";
+import CollectionsIcon from "@mui/icons-material/Collections"; // For Create Collection
 import ExploreIcon from "@mui/icons-material/Explore";
 import DnsIcon from "@mui/icons-material/Dns";
 import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
-import HourglassEmptyIcon from "@mui/icons-material/HourglassEmpty";
+import ShareIcon from "@mui/icons-material/Share"; // For Share NFT
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import HowToVoteIcon from "@mui/icons-material/HowToVote";
 import LockIcon from "@mui/icons-material/Lock";
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import ShareNFTModal from "../ShareNFTModal/ShareNFTModal";
-const Sidebar = () => {
 
+const Sidebar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
+
   return (
     <div className="sidebar">
       <div className="top">
@@ -30,63 +31,60 @@ const Sidebar = () => {
         <ul>
           <p className="title">MAIN</p>
           <li>
-          <Link to="/Dashboard" style={{ textDecoration: "none" }}>
-            <HomeIcon className="icon" />
-            <span>Home</span>
-          </Link>
+            <Link to="/Dashboard" style={{ textDecoration: "none" }}>
+              <HomeIcon className="icon" />
+              <span>Home</span>
+            </Link>
           </li>
           <p className="title">LISTS</p>
           <li>
-          <Link to="/BinanceChart" style={{ textDecoration: "none" }}>
-            <ShowChartIcon className="icon" />
-            <span>Live Pricing</span>
-          </Link>
+            <Link to="/BinanceChart" style={{ textDecoration: "none" }}>
+              <ShowChartIcon className="icon" />
+              <span>Live Pricing</span>
+            </Link>
           </li>
           <li>
-          <Link to="/NFTdetail" style={{ textDecoration: "none" }}>
-            <ShowChartIcon className="icon" />
-            <span>NFTdetail</span>
-          </Link>
+            <Link to="/NFTdetail" style={{ textDecoration: "none" }}>
+              <ExploreIcon className="icon" />
+              <span>NFT Detail</span>
+            </Link>
           </li>
           <li>
-          <Link to="/CreatePageDashboard" style={{ textDecoration: "none" }}>
-            <SmartToyIcon className="icon" />
-            <span>Create Collection</span>
-          </Link>
-          </li>
-
-          <li>
-          <Link to="/NFTsPage" style={{ textDecoration: "none" }}>
-            <ExploreIcon className="icon" />
-            <span>NFTs</span>
-          </Link>
+            <Link to="/CreatePageDashboard" style={{ textDecoration: "none" }}>
+              <CollectionsIcon className="icon" />
+              <span>Create Collection</span>
+            </Link>
           </li>
           <li>
-          <Link to="/PoolTable" style={{ textDecoration: "none" }}>
-            <DnsIcon className="icon" />
-            <span>Farm</span>
+            <Link to="/NFTsPage" style={{ textDecoration: "none" }}>
+              <ExploreIcon className="icon" />
+              <span>NFTs</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/PoolTable" style={{ textDecoration: "none" }}>
+              <DnsIcon className="icon" />
+              <span>Farm</span>
             </Link>
           </li>
           <p className="title">SERVICE</p>
-
           <li>
-          <Link to="/SwapComponent" style={{ textDecoration: "none" }}>
-            <SwapHorizIcon className="icon" />
-            <span>Swap</span>
-          </Link>
+            <Link to="/SwapComponent" style={{ textDecoration: "none" }}>
+              <SwapHorizIcon className="icon" />
+              <span>Swap</span>
+            </Link>
           </li>
           <li onClick={openModal}>
-            <HourglassEmptyIcon className="icon" />
+            <ShareIcon className="icon" />
             <span>Share NFT</span>
           </li>
           <ShareNFTModal isOpen={isModalOpen} onClose={closeModal} />
-
           <p className="title">USER</p>
           <li>
-          <Link to="/AccountDashboard" style={{ textDecoration: "none" }}>
-            <AccountCircleIcon className="icon" />
-            <span>Profile</span>
-          </Link>
+            <Link to="/AccountDashboard" style={{ textDecoration: "none" }}>
+              <AccountCircleIcon className="icon" />
+              <span>Profile</span>
+            </Link>
           </li>
           <li>
             <HowToVoteIcon className="icon" />
@@ -99,14 +97,8 @@ const Sidebar = () => {
         </ul>
       </div>
       <div className="bottom">
-        <div
-          className="colorOption"
-          onClick=''
-        ></div>
-        <div
-          className="colorOption"
-          onClick=''
-        ></div>
+        <div className="colorOption" onClick=''></div>
+        <div className="colorOption" onClick=''></div>
       </div>
     </div>
   );
